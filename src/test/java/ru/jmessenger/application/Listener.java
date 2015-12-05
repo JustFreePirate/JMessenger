4779b16d79d4a0d4850b2198bb59e64b6ad2d3c7
GKerfImf <gkerfimf@gmail.com>
2015-12-05 19:52:06 +0300

    Draft Listener with Deq

--- src/test/java/ru/jmessenger/application/Listener.java
+++ src/test/java/ru/jmessenger/application/Listener.java
@@ -7,18 +7,20 @@ import javax.net.ssl.SSLSocket;
 import java.io.InputStream;
 import java.net.SocketTimeoutException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayDeque;
 
 /**
  * Created by Сергей on 04.12.2015.
  */
 public class Listener implements Runnable {
-
     private Thread thread;
     private static final int BUFF_LEN = 1024 * 5;
     private InputStream inputStream;
-    private PackageService packageService;
+    //private PackageService packageService;
 
-    public Listener(SSLSocket sslSocket){
+    private ArrayDeque<Package> arrayDeque;
+
+    public Listener(SSLSocket sslSocket, ArrayDeque<Package> arrayDeque){
         try {
             inputStream = sslSocket.getInputStream();
 
@@ -26,7 +28,9 @@ public class Listener implements Runnable {
             exception.printStackTrace();
         }
 
-        packageService = new PackageService();
+        this.arrayDeque = arrayDeque;
+
+        //packageService = new PackageService();
         thread = new Thread(this, "ListenerThread");
         thread.start();
     }
@@ -46,7 +50,8 @@ public class Listener implements Runnable {
                     if ((r = inputStream.read(buf)) > 0) {
                         try {
                             ru.jmessenger.application.common.Package receivedPack = Package.deserialize(buf);
-                            packageService.processPackage(receivedPack);
+                            //packageService.processPackage(receivedPack);
+                            arrayDeque.add(receivedPack);
                         } catch (Exception e) {
                             System.out.println("failed to deserialize");
                         }
@@ -59,28 +64,28 @@ public class Listener implements Runnable {
         }
     }
 
-    class PackageService {
-        Package currentPackage;
-        SimpleDateFormat formatter;
-
-        PackageService() {
-            formatter = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
-        }
-
-        //обрабатывает пришедший пакет
-        void processPackage(Package pack) {
-            currentPackage = pack;
-
-            PackageType packType = pack.getType();
-            switch (packType) {
-                case REQ_SEND_MESSAGE:
-                    System.out.println("From: " + pack.getLogin() + " " + formatter.format(pack.getDate()) +
-                            "\n" + "-> " + pack.getMessage());
-                    break;
-
-                default:
-                    System.out.println(packType.name());
-            }
-        }
-    }
+//    class PackageService {
+//        Package currentPackage;
+//        SimpleDateFormat formatter;
+//
+//        PackageService() {
+//            formatter = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+//        }
+//
+//        //обрабатывает пришедший пакет
+//        void processPackage(Package pack) {
+//            currentPackage = pack;
+//
+//            PackageType packType = pack.getType();
+//            switch (packType) {
+//                case REQ_SEND_MESSAGE:
+//                    System.out.println("From: " + pack.getLogin() + " " + formatter.format(pack.getDate()) +
+//                            "\n" + "-> " + pack.getMessage());
+//                    break;
+//
+//                default:
+//                    System.out.println(packType.name());
+//            }
+//        }
+//    }
 }
